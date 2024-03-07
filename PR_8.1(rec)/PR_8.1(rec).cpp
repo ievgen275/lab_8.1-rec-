bc63a023bc5db085384ab2aff862d1c5c962dff0
ievgen275 <ievgen3435@gmail.com>
2024-03-07 23:00:49 +0200

    first commit

---
+++ PR_8.1(rec)/PR_8.1(rec).cpp
@@ -0,0 +1,71 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int Count(char* str, int i)
+{
+    if (strlen(str) < 2)
+        return 0;
+
+    if (str[i + 1] != 0)
+    {
+        if ((str[i] == 's' && str[i + 1] == 'q') ||
+            (str[i] == 'q' && str[i + 1] == 's'))
+            return 1 + Count(str, i + 2);
+        else
+            return Count(str, i + 1);
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+char* Change(char* dest, const char* str, char* t, int i)
+{
+    if (str[i + 1] != 0)
+    {
+        if ((str[i] == 's' && str[i + 1] == 'q') ||
+            (str[i] == 'q' && str[i + 1] == 's'))
+        {
+            *t++ = '*';
+            *t++ = '*';
+            *t++ = '*';
+            return Change(dest, str, t, i + 2);
+        }
+        else
+        {
+            *t++ = str[i++];
+            *t = '\0';
+            return Change(dest, str, t, i);
+        }
+    }
+    else
+    {
+        *t++ = str[i++];
+        *t++ = str[i++];
+        *t = '\0';
+        return dest;
+    }
+}
+
+int main()
+{
+    char str[101];
+    cout << "Enter string:" << endl;
+    cin.getline(str, 100);
+
+    cout << "String contained " << Count(str, 0) << " groups of 'SQ or QS'" << endl;
+
+    char* dest = new char[151];
+    dest[0] = '\0';
+    char* result = Change(dest, str, dest, 0);
+
+    cout << "Modified string (param) : " << str << endl;
+    cout << "Modified string (result): " << result << endl;
+
+    delete[] dest;
+    return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,29 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../PR_8.1(rec)/PR_8.1(rec).cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			char str[] = "abcsqdefg";
+			Assert::AreEqual(1, Count(str, 0));
+		}
+		TEST_METHOD(TestMethod2)
+		{
+			char* dest = new char[151];
+			dest[0] = '\0';
+			char str[] = "sqabcsqdefgqsq";
+
+			char* result = Change(dest, str, dest, 0);
+			Assert::AreEqual("***abc***defg***q", result);
+			delete[] result;
+		}
+	};
+}
